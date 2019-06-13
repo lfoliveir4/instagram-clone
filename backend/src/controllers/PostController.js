4a5d9f7d13492c1fee8ff12d8c386f76ac4cbea3
Luis Filipe Alves de Oliveira <luis.felipi@hotmail.com>
2019-06-13 09:32:04 -0300

    First Commit

---
+++ backend/src/controllers/PostController.js
@@ -0,0 +1,45 @@
+const Post = require('../models/Post')
+const sharp = require('sharp')
+const path = require('path')
+const fs = require('fs')
+
+module.exports ={
+    async index(req, res) {
+        const posts = await Post.find().sort('-createdAt')
+
+        return res.json(posts)
+
+    },
+
+    async store(req, res) {
+        const { author, description, hashtags, place } = req.body
+        const { filename: image } = req.file
+
+        const [name] = image.split('.')
+        const fileName = `${name}.jpg`
+
+        await sharp(req.file.path)
+        .resize(500)
+        .jpeg({ quality: 70 })
+        .toFile(
+            path.resolve(req.file.destination, 'resized', fileName)
+        )
+
+        fs.unlinkSync(req.file.path)
+
+        const post = await Post.create({
+            author, 
+            description, 
+            hashtags, 
+            place,
+            image: fileName
+
+        })
+
+        req.io.emit('post', post)
+
+
+
+        return res.json(post)
+    }
+}
